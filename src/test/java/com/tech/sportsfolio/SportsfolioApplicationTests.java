5dd149b44446c85899e2b567929af5e50ef56583
Adarsh Verma <adarshverma013@gmail.com>
2025-01-19 23:36:44 +0530

    initalizing project

---
+++ src/test/java/com/tech/sportsfolio/SportsfolioApplicationTests.java
@@ -0,0 +1,13 @@
+package com.tech.sportsfolio;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SportsfolioApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
